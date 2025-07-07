408c95b8b6cb6ee059db792bd8011db3ee871af8
kitakkun <48154936+kitakkun@users.noreply.github.com>
2025-07-08 01:03:55 +0900

    Refactor: remove workaround code for JVM application

--- app-desktop/src/main/kotlin/io/github/droidkaigi/confsched/Main.kt
+++ app-desktop/src/main/kotlin/io/github/droidkaigi/confsched/Main.kt
@@ -1,23 +1,15 @@
 package io.github.droidkaigi.confsched
 
 import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.produceState
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.LifecycleOwner
-import androidx.lifecycle.LifecycleRegistry
-import androidx.lifecycle.compose.LocalLifecycleOwner
 import androidx.navigationevent.NavigationEventDispatcher
 import androidx.navigationevent.NavigationEventDispatcherOwner
 import androidx.navigationevent.compose.LocalNavigationEventDispatcherOwner
 import dev.zacsweers.metro.createGraph
 import dev.zacsweers.metro.createGraphFactory
-import io.github.droidkaigi.confsched.data.JvmDataGraph
-import kotlinx.coroutines.delay
 
 fun main() = application {
     val graph: JvmAppGraph = createGraphFactory<JvmAppGraph.Factory>()
@@ -30,20 +22,10 @@ fun main() = application {
             height = 800.dp,
         )
     ) {
-        // Workaround for "component must be showing on the screen to determine its location" error
-        val visible by produceState(false) {
-            delay(1000L)
-            value = true
-        }
-
-        if (visible) {
-            // workaround for missing CompositionLocalProvider
-            CompositionLocalProvider(LocalLifecycleOwner provides FakeLifecycleOwner()) {
-                CompositionLocalProvider(LocalNavigationEventDispatcherOwner provides FakeNavigationEventDispatcherOwner()) {
-                    with(graph) {
-                        KaigiApp()
-                    }
-                }
+        // workaround for java.lang.NullPointerException at androidx.navigationevent.compose.LocalNavigationEventDispatcherOwner.getCurrent
+        CompositionLocalProvider(LocalNavigationEventDispatcherOwner provides FakeNavigationEventDispatcherOwner()) {
+            with(graph) {
+                KaigiApp()
             }
         }
     }
@@ -51,8 +33,4 @@ fun main() = application {
 
 private class FakeNavigationEventDispatcherOwner : NavigationEventDispatcherOwner {
     override val navigationEventDispatcher: NavigationEventDispatcher get() = NavigationEventDispatcher()
-}
-
-private class FakeLifecycleOwner : LifecycleOwner {
-    override val lifecycle: Lifecycle = LifecycleRegistry(this)
 }
