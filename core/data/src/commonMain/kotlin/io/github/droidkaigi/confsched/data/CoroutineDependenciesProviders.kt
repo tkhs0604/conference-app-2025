86181e223b66f3b0660daac05b05045e69a48ce1
kitakkun <48154936+kitakkun@users.noreply.github.com>
2025-07-17 20:57:57 +0900

    Fix: compilation error due to missing `@Provides` annotation

--- core/data/src/commonMain/kotlin/io/github/droidkaigi/confsched/data/CoroutineDependenciesProviders.kt
+++ core/data/src/commonMain/kotlin/io/github/droidkaigi/confsched/data/CoroutineDependenciesProviders.kt
@@ -1,6 +1,7 @@
 package io.github.droidkaigi.confsched.data
 
 import dev.zacsweers.metro.ContributesTo
+import dev.zacsweers.metro.Provides
 import io.github.droidkaigi.confsched.data.annotations.IoDispatcher
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
@@ -8,5 +9,6 @@ import kotlinx.coroutines.Dispatchers
 @ContributesTo(DataScope::class)
 public interface CoroutineDependenciesProviders {
     @IoDispatcher
+    @Provides
     public fun ioDispatcher(): CoroutineDispatcher = Dispatchers.IO
 }
