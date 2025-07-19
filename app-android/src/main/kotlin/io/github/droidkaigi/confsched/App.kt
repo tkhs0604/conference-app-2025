d9bf7dfee4c26d65054f6d20b0656289e1a7cd8f
kitakkun <48154936+kitakkun@users.noreply.github.com>
2025-07-19 22:36:23 +0900

    Refactor: remove unused import

--- app-android/src/main/kotlin/io/github/droidkaigi/confsched/App.kt
+++ app-android/src/main/kotlin/io/github/droidkaigi/confsched/App.kt
@@ -3,7 +3,6 @@ package io.github.droidkaigi.confsched
 import android.app.Application
 import android.content.Context
 import dev.zacsweers.metro.createGraphFactory
-import io.github.droidkaigi.confsched.data.AndroidDataGraph
 
 class App : Application() {
     val appGraph: AppGraph by lazy {
