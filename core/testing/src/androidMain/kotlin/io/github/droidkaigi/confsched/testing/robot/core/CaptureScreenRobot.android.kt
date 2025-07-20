93a4be56cb7c2beeec949ae058daded1d3ae3ac8
kitakkun <48154936+kitakkun@users.noreply.github.com>
2025-07-20 18:16:04 +0900

    Fix: Roborazzi screenshots not recognized by IDE plugin
    Include the fully qualified name of the test class at the beginning of each file name

--- core/testing/src/androidMain/kotlin/io/github/droidkaigi/confsched/testing/robot/core/CaptureScreenRobot.android.kt
+++ core/testing/src/androidMain/kotlin/io/github/droidkaigi/confsched/testing/robot/core/CaptureScreenRobot.android.kt
@@ -1,8 +1,17 @@
 package io.github.droidkaigi.confsched.testing.robot.core
 
 import androidx.compose.ui.test.SemanticsNodeInteraction
+import com.github.takahirom.roborazzi.DefaultFileNameGenerator
+import com.github.takahirom.roborazzi.InternalRoborazziApi
 import com.github.takahirom.roborazzi.captureRoboImage
 
+@OptIn(InternalRoborazziApi::class)
 actual fun SemanticsNodeInteraction.captureNodeWithDescription(description: String) {
-    this.captureRoboImage("$description.png")
+    val filePath = DefaultFileNameGenerator.generateFilePath()
+        .split(".")
+        .dropLast(2) // drop method name and extension
+        .joinToString(".")
+        .plus(" - $description.png")
+    println("Capture screen: $filePath")
+    this.captureRoboImage(filePath)
 }
