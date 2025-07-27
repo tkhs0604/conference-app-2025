b4a0e4b95014ba9729697a93ca26cbfae909305e
touyou <465697+touyou@users.noreply.github.com>
2025-07-27 11:45:15 +0900

    feat: set tab icon

--- app-ios/Native/Sources/Root/RootScreen.swift
+++ app-ios/Native/Sources/Root/RootScreen.swift
@@ -3,38 +3,53 @@ import SwiftUI
 import HomeFeature
 
 private enum TabType {
-    case timeline
+    case timetable
     case map
     case favorite
     case info
     case profileCard
+
+    internal func tabImageName(_ selectedTab: TabType) -> String {
+        switch self {
+        case .timetable:
+            return selectedTab == self ? "ic_timetable.fill" : "ic_timetable"
+        case .map:
+            return selectedTab == self ? "ic_map.fill" : "ic_map"
+        case .favorite:
+            return selectedTab == self ? "ic_fav.fill" : "ic_fav"
+        case .info:
+            return selectedTab == self ? "ic_info.fill" : "ic_info"
+        case .profileCard:
+            return selectedTab == self ? "ic_profileCard.fill" : "ic_profileCard"
+        }
+    }
 }
 
 public struct RootScreen: View {
     @Environment(\.scenePhase) private var scenePhase
-    @State private var selectedTab: TabType = .timeline
+    @State private var selectedTab: TabType = .timetable
     private let presenter = RootPresenter()
 
     public init() {}
 
     public var body: some View {
         TabView(selection: $selectedTab) {
-            Tab("Timeline", systemImage: "calendar", value: .timeline) {
+            Tab("Timeline", image: TabType.timetable.tabImageName(selectedTab), value: .timetable) {
                 HomeScreen()
             }
-            Tab("Map", systemImage: "map", value: .map) {
+            Tab("Map", image: TabType.map.tabImageName(selectedTab), value: .map) {
                 // TODO: Replace correct screen
                 HomeScreen()
             }
-            Tab("Favorite", systemImage: "heart", value: .favorite) {
+            Tab("Favorite", image: TabType.favorite.tabImageName(selectedTab), value: .favorite) {
                 // TODO: Replace correct screen
                 HomeScreen()
             }
-            Tab("Info", systemImage: "info.circle", value: .info) {
+            Tab("Info", image: TabType.info.tabImageName(selectedTab), value: .info) {
                 // TODO: Replace correct screen
                 HomeScreen()
             }
-            Tab("Profile Card", systemImage: "person.crop.square.on.square.angled", value: .info) {
+            Tab("Profile Card", image: TabType.profileCard.tabImageName(selectedTab), value: .info) {
                 // TODO: Replace correct screen
                 HomeScreen()
             }
