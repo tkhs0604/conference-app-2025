0d4a6ad918e15dc366c99d7f59d8a58f2dcd6342
kitakkun <48154936+kitakkun@users.noreply.github.com>
2025-07-31 03:56:51 +0900

    fix: missing actual implementation of createTestAppGraph for native

---
+++ core/testing/src/nativeMain/kotlin/io/github/droidkaigi/confsched/testing/di/TestAppGraph.native.kt
@@ -0,0 +1,25 @@
+package io.github.droidkaigi.confsched.testing.di
+
+import dev.zacsweers.metro.AppScope
+import dev.zacsweers.metro.DependencyGraph
+import dev.zacsweers.metro.createGraph
+import io.github.droidkaigi.confsched.data.DataScope
+import io.github.droidkaigi.confsched.data.contributors.DefaultContributorsApiClient
+import io.github.droidkaigi.confsched.data.sessions.DefaultSessionsApiClient
+import kotlinx.coroutines.CoroutineDispatcher
+
+@DependencyGraph(
+    scope = AppScope::class,
+    additionalScopes = [DataScope::class],
+    isExtendable = true,
+    excludes = [
+        DefaultSessionsApiClient::class,
+        DefaultContributorsApiClient::class,
+        CoroutineDispatcher::class,
+    ],
+)
+interface NativeTestAppGraph : TestAppGraph
+
+actual fun createTestAppGraph(): TestAppGraph {
+    return createGraph<NativeTestAppGraph>()
+}
