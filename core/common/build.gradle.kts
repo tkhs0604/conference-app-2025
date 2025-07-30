26dbee18d8eae4de1a7d405869e8a30e7eeb6010
kitakkun <48154936+kitakkun@users.noreply.github.com>
2025-07-31 03:25:38 +0900

    chore: apply kmp.ios plugin to common module

--- core/common/build.gradle.kts
+++ core/common/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.jetbrainsCompose)
     alias(libs.plugins.composeCompiler)
     id("droidkaigi.primitive.kmp")
+    id("droidkaigi.primitive.kmp.ios")
     alias(libs.plugins.metro)
 }
 
