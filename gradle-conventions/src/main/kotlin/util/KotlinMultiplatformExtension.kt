12ec54cf0849849defaeac2c41d7044e74bdde36
kitakkun <48154936+kitakkun@users.noreply.github.com>
2025-07-31 03:41:09 +0900

    chore: add utility function to configure androidJvm shared source set

--- gradle-conventions/src/main/kotlin/util/KotlinMultiplatformExtension.kt
+++ gradle-conventions/src/main/kotlin/util/KotlinMultiplatformExtension.kt
@@ -1,10 +1,12 @@
 package util
 
 import com.android.build.api.dsl.KotlinMultiplatformAndroidLibraryExtension
+import org.gradle.api.NamedDomainObjectContainer
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.DependencyHandlerScope
 import org.gradle.kotlin.dsl.getByType
 import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
+import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet
 
 internal fun Project.kotlin(
     block: KotlinMultiplatformExtension.() -> Unit
@@ -23,3 +25,10 @@ internal fun DependencyHandlerScope.commonMainImplementation(
 ) {
     add("commonMainImplementation", dependencyNotation)
 }
+
+val NamedDomainObjectContainer<KotlinSourceSet>.androidJvmMain
+    get() = maybeCreate("androidJvmMain").also {
+        it.dependsOn(getByName("commonMain"))
+        getByName("androidMain").dependsOn(it)
+        getByName("jvmMain").dependsOn(it)
+    }
