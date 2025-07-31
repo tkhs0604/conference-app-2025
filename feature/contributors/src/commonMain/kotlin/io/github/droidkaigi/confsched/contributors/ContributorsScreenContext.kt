58e67547e7eaa7fe7846d45c9d4215471745bd5c
kitakkun <48154936+kitakkun@users.noreply.github.com>
2025-07-31 21:19:05 +0900

    feat: add contributorsQueryKey to ContributorsScreenContext

--- feature/contributors/src/commonMain/kotlin/io/github/droidkaigi/confsched/contributors/ContributorsScreenContext.kt
+++ feature/contributors/src/commonMain/kotlin/io/github/droidkaigi/confsched/contributors/ContributorsScreenContext.kt
@@ -3,9 +3,12 @@ package io.github.droidkaigi.confsched.contributors
 import dev.zacsweers.metro.AppScope
 import dev.zacsweers.metro.ContributesGraphExtension
 import io.github.droidkaigi.confsched.common.scope.ContributorsScope
+import io.github.droidkaigi.confsched.model.contributors.ContributorsQueryKey
 
 @ContributesGraphExtension(ContributorsScope::class)
 interface ContributorsScreenContext {
+    val contributorsQueryKey: ContributorsQueryKey
+
     @ContributesGraphExtension.Factory(AppScope::class)
     fun interface Factory {
         fun createContributorsScreenContext(): ContributorsScreenContext
