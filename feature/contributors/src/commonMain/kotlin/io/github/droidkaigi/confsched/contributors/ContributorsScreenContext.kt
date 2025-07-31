35075679688dd9dd74ba02c535b8de611f381a5e
kitakkun <48154936+kitakkun@users.noreply.github.com>
2025-07-31 21:19:52 +0900

    feat: add missing ScreenContext inteface to ContributorsScreenContext

--- feature/contributors/src/commonMain/kotlin/io/github/droidkaigi/confsched/contributors/ContributorsScreenContext.kt
+++ feature/contributors/src/commonMain/kotlin/io/github/droidkaigi/confsched/contributors/ContributorsScreenContext.kt
@@ -3,10 +3,11 @@ package io.github.droidkaigi.confsched.contributors
 import dev.zacsweers.metro.AppScope
 import dev.zacsweers.metro.ContributesGraphExtension
 import io.github.droidkaigi.confsched.common.scope.ContributorsScope
+import io.github.droidkaigi.confsched.context.ScreenContext
 import io.github.droidkaigi.confsched.model.contributors.ContributorsQueryKey
 
 @ContributesGraphExtension(ContributorsScope::class)
-interface ContributorsScreenContext {
+interface ContributorsScreenContext : ScreenContext {
     val contributorsQueryKey: ContributorsQueryKey
 
     @ContributesGraphExtension.Factory(AppScope::class)
