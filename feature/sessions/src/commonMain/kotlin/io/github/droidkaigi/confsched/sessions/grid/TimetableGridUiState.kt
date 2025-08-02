87a3a0515c7912ef614cdf044a6305dd23ca3a05
mikan <29175998+mikanIchinose@users.noreply.github.com>
2025-08-03 02:39:21 +0900

    feat(sessions): implement TimetableGrid and related state management

--- feature/sessions/src/commonMain/kotlin/io/github/droidkaigi/confsched/sessions/grid/TimetableGridUiState.kt
+++ feature/sessions/src/commonMain/kotlin/io/github/droidkaigi/confsched/sessions/grid/TimetableGridUiState.kt
@@ -1,6 +1,5 @@
-package io.github.droidkaigi.confsched.sessions.section
+package io.github.droidkaigi.confsched.sessions.grid
 
 import io.github.droidkaigi.confsched.model.sessions.Timetable
 
 data class TimetableGridUiState(val timetable: Timetable)
-
