74f08b84af7894158b0a7846f7457f6b363d0e09
kitakkun <48154936+kitakkun@users.noreply.github.com>
2025-08-02 14:21:22 +0900

    feat: add missing declarations for iOS target

---
+++ app-shared/src/iosMain/kotlin/io/github/droidkaigi/confsched/IosAppGraph.kt
@@ -0,0 +1,12 @@
+package io.github.droidkaigi.confsched
+
+import dev.zacsweers.metro.AppScope
+import dev.zacsweers.metro.DependencyGraph
+import io.github.droidkaigi.confsched.data.DataScope
+
+@DependencyGraph(
+    scope = AppScope::class,
+    additionalScopes = [DataScope::class],
+    isExtendable = true,
+)
+interface IosAppGraph : AppGraph
---
+++ app-shared/src/iosMain/kotlin/io/github/droidkaigi/confsched/KaigiAppUi.ios.kt
@@ -0,0 +1,8 @@
+package io.github.droidkaigi.confsched
+
+import androidx.compose.runtime.Composable
+
+context(appGraph: AppGraph)
+@Composable
+actual fun KaigiAppUi() {
+}
