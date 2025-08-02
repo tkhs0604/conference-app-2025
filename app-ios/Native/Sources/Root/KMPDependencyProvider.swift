c25316f405f49c9be1fe6d6c02aacac6c596e22c
kitakkun <48154936+kitakkun@users.noreply.github.com>
2025-08-02 22:41:37 +0900

    feat: add KMPDependencyProvider to manage IosAppGraph as a singleton

---
+++ app-ios/Native/Sources/Root/KMPDependencyProvider.swift
@@ -0,0 +1,10 @@
+@preconcurrency import shared
+
+public struct KMPDependencyProvider: Sendable {
+    public static let shared: KMPDependencyProvider = .init()
+
+    public let appGraph: shared.IosAppGraph
+    private init() {
+        self.appGraph = createIosAppGraph()
+    }
+}
