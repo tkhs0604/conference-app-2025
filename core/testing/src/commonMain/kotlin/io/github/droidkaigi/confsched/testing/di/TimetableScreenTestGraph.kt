bed1e6d0c2d8277dc4ddd0f4def4f9994b64fa1f
kitakkun <48154936+kitakkun@users.noreply.github.com>
2025-08-03 23:50:10 +0900

    efactor: publish TimetableScreenTest dependencies via TimetableScreenTestGraph instead of TestAppGraph

---
+++ core/testing/src/commonMain/kotlin/io/github/droidkaigi/confsched/testing/di/TimetableScreenTestGraph.kt
@@ -0,0 +1,15 @@
+package io.github.droidkaigi.confsched.testing.di
+
+import dev.zacsweers.metro.Provider
+import dev.zacsweers.metro.Provides
+import io.github.droidkaigi.confsched.sessions.TimetableScreenContext
+import io.github.droidkaigi.confsched.testing.robot.sessions.TimetableScreenRobot
+
+interface TimetableScreenTestGraph : TimetableScreenContext.Factory {
+    val timetableScreenRobotProvider: Provider<TimetableScreenRobot>
+
+    @Provides
+    fun provideTimetableScreenContext(): TimetableScreenContext {
+        return createTimetableScreenContext()
+    }
+}
