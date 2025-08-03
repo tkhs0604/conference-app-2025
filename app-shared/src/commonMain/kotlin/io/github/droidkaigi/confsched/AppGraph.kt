c243e6eed6b44baf9f67cce495ca78346c0fb0c5
kuwa <h.kuwabara1103@gmail.com>
2025-08-03 20:11:09 +0900

    refacor: annotate DefaultBuildConfigProvider with @ContributesBinding directly

--- app-shared/src/commonMain/kotlin/io/github/droidkaigi/confsched/AppGraph.kt
+++ app-shared/src/commonMain/kotlin/io/github/droidkaigi/confsched/AppGraph.kt
@@ -1,11 +1,8 @@
 package io.github.droidkaigi.confsched
 
-import dev.zacsweers.metro.Provides
 import io.github.droidkaigi.confsched.about.AboutScreenContext
-import io.github.droidkaigi.confsched.buildconfig.DefaultBuildConfigProvider
 import io.github.droidkaigi.confsched.contributors.ContributorsScreenContext
 import io.github.droidkaigi.confsched.favorites.FavoritesScreenContext
-import io.github.droidkaigi.confsched.model.buildconfig.BuildConfigProvider
 import io.github.droidkaigi.confsched.sessions.SearchScreenContext
 import io.github.droidkaigi.confsched.sessions.TimetableItemDetailScreenContext
 import io.github.droidkaigi.confsched.sessions.TimetableScreenContext
@@ -16,10 +13,4 @@ interface AppGraph :
     SearchScreenContext.Factory,
     ContributorsScreenContext.Factory,
     FavoritesScreenContext.Factory,
-    AboutScreenContext.Factory {
-
-    @Provides
-    fun provideBuildConfigProvider(): BuildConfigProvider {
-        return DefaultBuildConfigProvider()
-    }
-}
+    AboutScreenContext.Factory
