742df1a5acb89d80d90c6743860b816ecefc59a0
kitakkun <48154936+kitakkun@users.noreply.github.com>
2025-08-03 23:24:58 +0900

    feat: make TimetableScreenTest runnable on iOS

--- core/testing/src/iosMain/kotlin/io/github/droidkaigi/confsched/testing/di/TestAppGraph.ios.kt
+++ core/testing/src/iosMain/kotlin/io/github/droidkaigi/confsched/testing/di/TestAppGraph.ios.kt
@@ -1,12 +1,39 @@
 package io.github.droidkaigi.confsched.testing.di
 
+import androidx.datastore.core.DataStore
+import androidx.datastore.core.handlers.ReplaceFileCorruptionHandler
+import androidx.datastore.preferences.core.PreferenceDataStoreFactory
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.emptyPreferences
 import dev.zacsweers.metro.AppScope
+import dev.zacsweers.metro.Binds
 import dev.zacsweers.metro.DependencyGraph
+import dev.zacsweers.metro.Provides
+import dev.zacsweers.metro.SingleIn
 import dev.zacsweers.metro.createGraph
 import io.github.droidkaigi.confsched.data.DataScope
+import io.github.droidkaigi.confsched.data.SessionCacheDataStoreQualifier
+import io.github.droidkaigi.confsched.data.UserDataStoreQualifier
+import io.github.droidkaigi.confsched.data.annotations.IoDispatcher
 import io.github.droidkaigi.confsched.data.contributors.DefaultContributorsApiClient
+import io.github.droidkaigi.confsched.data.core.DataStorePathProducer
+import io.github.droidkaigi.confsched.data.core.defaultJson
 import io.github.droidkaigi.confsched.data.sessions.DefaultSessionsApiClient
+import io.github.droidkaigi.confsched.data.sessions.DefaultTimetableSubscriptionKey
+import io.github.droidkaigi.confsched.data.user.DefaultFavoriteTimetableIdsSubscriptionKey
+import io.github.droidkaigi.confsched.data.user.DefaultFavoriteTimetableItemIdMutationKey
+import io.github.droidkaigi.confsched.model.data.FavoriteTimetableIdsSubscriptionKey
+import io.github.droidkaigi.confsched.model.data.FavoriteTimetableItemIdMutationKey
+import io.github.droidkaigi.confsched.model.data.TimetableSubscriptionKey
+import kotlinx.cinterop.ExperimentalForeignApi
 import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.serialization.json.Json
+import okio.Path.Companion.toPath
+import platform.Foundation.NSDocumentDirectory
+import platform.Foundation.NSFileManager
+import platform.Foundation.NSURL
+import platform.Foundation.NSUserDomainMask
 
 @DependencyGraph(
     scope = AppScope::class,
@@ -18,8 +45,73 @@ import kotlinx.coroutines.CoroutineDispatcher
         CoroutineDispatcher::class,
     ],
 )
-interface IosTestAppGraph : TestAppGraph
+interface IosTestAppGraph : TestAppGraph {
+    @Binds
+    val DefaultTimetableSubscriptionKey.bind: TimetableSubscriptionKey
+
+    @Binds
+    val DefaultFavoriteTimetableIdsSubscriptionKey.bind: FavoriteTimetableIdsSubscriptionKey
+
+    @Binds
+    val DefaultFavoriteTimetableItemIdMutationKey.bind: FavoriteTimetableItemIdMutationKey
+
+    @Provides
+    fun provideJson(): Json {
+        return defaultJson()
+    }
+
+    @SingleIn(DataScope::class)
+    @UserDataStoreQualifier
+    @Provides
+    fun provideDataStorePreferences(
+        dataStorePathProducer: DataStorePathProducer,
+        @IoDispatcher ioDispatcher: CoroutineDispatcher,
+    ): DataStore<Preferences> {
+        return PreferenceDataStoreFactory.createWithPath(
+            corruptionHandler = ReplaceFileCorruptionHandler({ emptyPreferences() }),
+            migrations = emptyList(),
+            scope = CoroutineScope(ioDispatcher),
+            produceFile = { dataStorePathProducer.producePath(DATA_STORE_PREFERENCE_FILE_NAME).toPath() },
+        )
+    }
+
+    @SingleIn(DataScope::class)
+    @SessionCacheDataStoreQualifier
+    @Provides
+    fun provideSessionCacheDataStore(
+        dataStorePathProducer: DataStorePathProducer,
+        @IoDispatcher ioDispatcher: CoroutineDispatcher,
+    ): DataStore<Preferences> {
+        return PreferenceDataStoreFactory.createWithPath(
+            corruptionHandler = ReplaceFileCorruptionHandler({ emptyPreferences() }),
+            migrations = emptyList(),
+            scope = CoroutineScope(ioDispatcher),
+            produceFile = { dataStorePathProducer.producePath(DATA_STORE_CACHE_PREFERENCE_FILE_NAME).toPath() },
+        )
+    }
+
+    @OptIn(ExperimentalForeignApi::class)
+    @Provides
+    fun providesDataStorePathProducer(): DataStorePathProducer {
+        return object : DataStorePathProducer {
+            override fun producePath(fileName: String): String {
+                val documentDirectory: NSURL? = NSFileManager.defaultManager.URLForDirectory(
+                    directory = NSDocumentDirectory,
+                    inDomain = NSUserDomainMask,
+                    appropriateForURL = null,
+                    create = false,
+                    error = null,
+                )
+                return requireNotNull(documentDirectory).path + "/$fileName"
+            }
+        }
+    }
+}
 
 actual fun createTestAppGraph(): TestAppGraph {
     return createGraph<IosTestAppGraph>()
 }
+
+// These constants are duplicated with those in DataStoreDependencyProviders.kt
+private const val DATA_STORE_PREFERENCE_FILE_NAME = "confsched2025.preferences_pb"
+private const val DATA_STORE_CACHE_PREFERENCE_FILE_NAME = "confsched2025.cache.preferences_pb"
