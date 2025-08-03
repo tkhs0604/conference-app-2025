de2e66a415b1525e2db78959a690d2e8d36af32b
kuwa <h.kuwabara1103@gmail.com>
2025-08-03 15:18:26 +0900

    feat: add buildkonfig

--- gradle-conventions/build.gradle.kts
+++ gradle-conventions/build.gradle.kts
@@ -4,4 +4,6 @@ plugins {
 
 dependencies {
     implementation(libs.bundles.plugins)
+    // Not documented in README, but required to import buildkonfig.compiler. https://github.com/yshrsmz/BuildKonfig/issues/227
+    implementation(libs.buildkonfigCompiler)
 }
