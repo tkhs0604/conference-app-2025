14e0ad4dcea01cefd0e36614bb7981b132b8e1a7
kitakkun <48154936+kitakkun@users.noreply.github.com>
2025-08-05 14:14:59 +0900

    feat: implement navigate and navigateToCalendarRegistration logic for iOS

--- app-shared/src/iosMain/kotlin/io/github/droidkaigi/confsched/IosExternalNavController.kt
+++ app-shared/src/iosMain/kotlin/io/github/droidkaigi/confsched/IosExternalNavController.kt
@@ -2,6 +2,12 @@ package io.github.droidkaigi.confsched
 
 import androidx.compose.runtime.Composable
 import io.github.droidkaigi.confsched.model.sessions.TimetableItem
+import kotlinx.datetime.toNSDate
+import platform.EventKit.EKEntityType
+import platform.EventKit.EKEvent
+import platform.EventKit.EKEventStore
+import platform.Foundation.NSURL
+import platform.UIKit.UIApplication
 
 @Composable
 actual fun rememberExternalNavController(): ExternalNavController {
@@ -10,11 +16,29 @@ actual fun rememberExternalNavController(): ExternalNavController {
 
 internal class IosExternalNavController : ExternalNavController {
     override fun navigate(url: String) {
-        TODO("Not yet implemented")
+        val nsUrl = NSURL.URLWithString(url)
+        if (nsUrl != null) {
+            UIApplication.sharedApplication.openURL(nsUrl)
+        } else {
+            println("Failed to navigate to URL: $url")
+        }
     }
 
     override fun navigateToCalendarRegistration(timetableItem: TimetableItem) {
-        TODO("Not yet implemented")
+        val eventStore = EKEventStore()
+
+        eventStore.requestAccessToEntityType(EKEntityType.EKEntityTypeEvent) { granted, error ->
+            if (granted) {
+                val event = EKEvent.eventWithEventStore(eventStore)
+                event.startDate = timetableItem.startsAt.toNSDate()
+                event.endDate = timetableItem.endsAt.toNSDate()
+                event.title = timetableItem.title.currentLangTitle
+                event.notes = timetableItem.url
+                event.location = timetableItem.room.name.currentLangTitle
+            } else {
+                println("Access to calendar not granted: $error")
+            }
+        }
     }
 
     override fun onShareClick(timetableItem: TimetableItem) {
