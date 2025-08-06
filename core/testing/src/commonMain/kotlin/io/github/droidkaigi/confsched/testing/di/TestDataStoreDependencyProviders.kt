9dbb1a330fd7ce0a2db32a57fc764ee1eddf2afa
kitakkun <48154936+kitakkun@users.noreply.github.com>
2025-08-07 00:58:50 +0900

    fix: use in-memory dataStore for testing

---
+++ core/testing/src/commonMain/kotlin/io/github/droidkaigi/confsched/testing/di/TestDataStoreDependencyProviders.kt
@@ -0,0 +1,44 @@
+package io.github.droidkaigi.confsched.testing.di
+
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.emptyPreferences
+import dev.zacsweers.metro.ContributesTo
+import dev.zacsweers.metro.Provides
+import io.github.droidkaigi.confsched.data.DataScope
+import io.github.droidkaigi.confsched.data.DataStoreDependencyProviders
+import io.github.droidkaigi.confsched.data.SessionCacheDataStoreQualifier
+import io.github.droidkaigi.confsched.data.UserDataStoreQualifier
+import io.github.droidkaigi.confsched.data.annotations.IoDispatcher
+import io.github.droidkaigi.confsched.data.core.DataStorePathProducer
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.updateAndGet
+
+@ContributesTo(DataScope::class, replaces = [DataStoreDependencyProviders::class])
+interface TestDataStoreDependencyProviders {
+    @UserDataStoreQualifier
+    @Provides
+    fun provideDataStorePreferences(
+        dataStorePathProducer: DataStorePathProducer,
+        @IoDispatcher ioDispatcher: CoroutineDispatcher,
+    ): DataStore<Preferences> {
+        return InMemoryDataStore(emptyPreferences())
+    }
+
+    @SessionCacheDataStoreQualifier
+    @Provides
+    fun provideSessionCacheDataStore(
+        dataStorePathProducer: DataStorePathProducer,
+        @IoDispatcher ioDispatcher: CoroutineDispatcher,
+    ): DataStore<Preferences> {
+        return InMemoryDataStore(emptyPreferences())
+    }
+}
+
+private class InMemoryDataStore<T>(initialValue: T) : DataStore<T> {
+    override val data = MutableStateFlow(initialValue)
+    override suspend fun updateData(
+        transform: suspend (t: T) -> T
+    ): T = data.updateAndGet { transform(it) }
+}
