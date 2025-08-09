d2998260215a7585b6b9d0bfc754ae75a6071057
kuwa <h.kuwabara1103@gmail.com>
2025-08-09 22:45:30 +0900

    feat: create staff screen

--- core/model/src/commonMain/kotlin/io/github/droidkaigi/confsched/model/staff/Staff.kt
+++ core/model/src/commonMain/kotlin/io/github/droidkaigi/confsched/model/staff/Staff.kt
@@ -1,9 +1,24 @@
 package io.github.droidkaigi.confsched.model.staff
 
+import kotlinx.collections.immutable.PersistentList
+import kotlinx.collections.immutable.toPersistentList
+
 data class Staff(
     val id: String,
     val name: String,
     val icon: String,
     val profileUrl: String?,
-)
+) {
+    companion object
+}
 
+fun Staff.Companion.fakes(): PersistentList<Staff> {
+    return (1..20).map {
+        Staff(
+            id = it.toString(),
+            name = "username $it",
+            icon = "https://placehold.jp/150x150.png",
+            profileUrl = "https://developer.android.com/",
+        )
+    }.toPersistentList()
+}
---
+++ feature/staff/src/commonMain/kotlin/io/github/droidkaigi/confsched/staff/component/StaffItem.kt
@@ -0,0 +1,79 @@
+package io.github.droidkaigi.confsched.staff.component
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import io.github.droidkaigi.confsched.droidkaigiui.KaigiPreviewContainer
+import io.github.droidkaigi.confsched.droidkaigiui.previewOverridePainter
+import io.github.droidkaigi.confsched.droidkaigiui.rememberAsyncImagePainter
+import io.github.droidkaigi.confsched.model.staff.Staff
+import io.github.droidkaigi.confsched.model.staff.fakes
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+private val staffIconShape = CircleShape
+
+@Composable
+fun StaffItem(
+    staff: Staff,
+    onStaffItemClick: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Row(
+        modifier = modifier
+            .clickable(enabled = !staff.profileUrl.isNullOrBlank()) {
+                onStaffItemClick()
+            }
+            .padding(horizontal = 16.dp, vertical = 10.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(23.dp),
+    ) {
+        Image(
+            painter = previewOverridePainter(
+                previewPainter = { rememberVectorPainter(image = Icons.Default.Person) },
+                painter = { rememberAsyncImagePainter(staff.icon) }
+            ),
+            contentDescription = staff.name,
+            modifier = Modifier
+                .size(52.dp)
+                .clip(staffIconShape)
+                .border(
+                    width = 1.dp,
+                    color = MaterialTheme.colorScheme.outline,
+                    shape = staffIconShape,
+                )
+        )
+        Text(
+            text = staff.name,
+            style = MaterialTheme.typography.bodyLarge,
+            maxLines = 2,
+            overflow = TextOverflow.Ellipsis,
+        )
+    }
+}
+
+@Composable
+@Preview
+private fun StaffItemPreview() {
+    KaigiPreviewContainer {
+        StaffItem(
+            staff = Staff.fakes().first(),
+            onStaffItemClick = {},
+        )
+    }
+}
