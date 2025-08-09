d1c63010fad477c65cac05c0d09e5f45e8c0abd3
kitakkun <48154936+kitakkun@users.noreply.github.com>
2025-08-09 21:26:33 +0900

    refactor: remove redundant 'binding' argument from `@ContributesBinding`

--- core/data/src/commonMain/kotlin/io/github/droidkaigi/confsched/data/about/DefaultLicensesQueryKey.kt
+++ core/data/src/commonMain/kotlin/io/github/droidkaigi/confsched/data/about/DefaultLicensesQueryKey.kt
@@ -2,13 +2,12 @@ package io.github.droidkaigi.confsched.data.about
 
 import dev.zacsweers.metro.ContributesBinding
 import dev.zacsweers.metro.Inject
-import dev.zacsweers.metro.binding
 import io.github.droidkaigi.confsched.common.scope.LicensesScope
 import io.github.droidkaigi.confsched.model.about.LicensesQueryKey
 import soil.query.QueryId
 import soil.query.buildQueryKey
 
-@ContributesBinding(LicensesScope::class, binding<LicensesQueryKey>())
+@ContributesBinding(LicensesScope::class)
 @Inject
 public class DefaultLicensesQueryKey(
     private val licensesJsonReader: LicensesJsonReader,
