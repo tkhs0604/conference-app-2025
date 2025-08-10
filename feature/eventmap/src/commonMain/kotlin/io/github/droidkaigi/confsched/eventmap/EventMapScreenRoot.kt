111b2afda79295d5157235cffe6dc90c0d133506
kitakkun <48154936+kitakkun@users.noreply.github.com>
2025-08-10 13:58:12 +0900

    feat: add empty EventMapScreenRoot

---
+++ feature/eventmap/src/commonMain/kotlin/io/github/droidkaigi/confsched/eventmap/EventMapScreenRoot.kt
@@ -0,0 +1,8 @@
+package io.github.droidkaigi.confsched.eventmap
+
+import androidx.compose.runtime.Composable
+
+context(screenContext: EventMapScreenContext)
+@Composable
+fun EventMapScreenRoot() {
+}
