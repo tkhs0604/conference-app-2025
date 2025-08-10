42d7b43cc5945c5a73dd91eedea80ce124ebc51b
kitakkun <48154936+kitakkun@users.noreply.github.com>
2025-08-10 12:21:26 +0900

    feat: implement EventMapQueryKey

---
+++ core/data/src/commonMain/kotlin/io/github/droidkaigi/confsched/data/eventmap/DefaultEventMapQueryKey.kt
@@ -0,0 +1,17 @@
+package io.github.droidkaigi.confsched.data.eventmap
+
+import dev.zacsweers.metro.ContributesBinding
+import dev.zacsweers.metro.Inject
+import io.github.droidkaigi.confsched.data.DataScope
+import io.github.droidkaigi.confsched.model.eventmap.EventMapQueryKey
+import soil.query.QueryId
+import soil.query.buildQueryKey
+
+@ContributesBinding(DataScope::class)
+@Inject
+public class DefaultEventMapQueryKey(
+    private val eventMapApiClient: EventMapApiClient,
+) : EventMapQueryKey by buildQueryKey(
+    id = QueryId("event_map_query_key"),
+    fetch = { eventMapApiClient.eventMapEvents() }
+)
