997f0c343c93a2eca8d04957ca37ab43c9274b19
kitakkun <48154936+kitakkun@users.noreply.github.com>
2025-08-10 23:57:12 +0900

    fix: use proper primaryColor and containerColor for RoomTheme

--- core/designsystem/src/commonMain/kotlin/io/github/droidkaigi/confsched/designsystem/theme/RoomTheme.kt
+++ core/designsystem/src/commonMain/kotlin/io/github/droidkaigi/confsched/designsystem/theme/RoomTheme.kt
@@ -8,36 +8,31 @@ import androidx.compose.ui.graphics.Color
 
 sealed interface RoomTheme {
     val primaryColor: Color
-    val containerColor: Color
+    val containerColor: Color get() = primaryColor.copy(alpha = 0.1f)
     val dimColor: Color
 
     data object Jellyfish : RoomTheme {
-        override val primaryColor = Color(0xFF45E761)
-        override val containerColor = Color(0xFF45E761).copy(alpha = 0.1f)
+        override val primaryColor = Color(0xFFBCC8F5)
         override val dimColor = Color(0xFF132417)
     }
 
     data object Koala : RoomTheme {
-        override val primaryColor = Color(0xFFFF974B)
-        override val containerColor = Color(0xFFFF974B).copy(alpha = 0.1f)
+        override val primaryColor = Color(0xFFDAA3ED)
         override val dimColor = Color(0xFF251C15)
     }
 
     data object Ladybug : RoomTheme {
-        override val primaryColor = Color(0xFFDDD33C)
-        override val containerColor = Color(0xFFDDD33C).copy(alpha = 0.1f)
+        override val primaryColor = Color(0xFFB19BFA)
         override val dimColor = Color(0xFF222213)
     }
 
     data object Meerkat : RoomTheme {
-        override val primaryColor = Color(0xFFFF53CF)
-        override val containerColor = Color(0xFFFF53CF).copy(alpha = 0.1f)
+        override val primaryColor = Color(0xFFB2F5FB)
         override val dimColor = Color(0xFF271A25)
     }
 
     data object Narwhal : RoomTheme {
-        override val primaryColor = Color(0xFF44ADE7)
-        override val containerColor = Color(0xFF44ADE7).copy(alpha = 0.1f)
+        override val primaryColor = Color(0xFF7EE5BD)
         override val dimColor = Color(0xFF121E25)
     }
 }
