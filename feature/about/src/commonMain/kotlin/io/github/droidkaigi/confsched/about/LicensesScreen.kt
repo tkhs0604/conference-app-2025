8575187262a8f8253fad4d3a10f38453a120c55f
kitakkun <48154936+kitakkun@users.noreply.github.com>
2025-08-12 00:00:41 +0900

    fix: enable pinnedScrollBehavior in LicensesScreen and fix contentPadding usage

--- feature/about/src/commonMain/kotlin/io/github/droidkaigi/confsched/about/LicensesScreen.kt
+++ feature/about/src/commonMain/kotlin/io/github/droidkaigi/confsched/about/LicensesScreen.kt
@@ -1,15 +1,16 @@
 package io.github.droidkaigi.confsched.about
 
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import com.mikepenz.aboutlibraries.Libs
 import com.mikepenz.aboutlibraries.ui.compose.m3.LibrariesContainer
 import io.github.droidkaigi.confsched.droidkaigiui.component.AnimatedTextTopAppBar
@@ -21,6 +22,8 @@ fun LicensesScreen(
     libraries: Libs?,
     onBackClick: () -> Unit,
 ) {
+    val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior()
+
     Scaffold(
         topBar = {
             AnimatedTextTopAppBar(
@@ -32,15 +35,17 @@ fun LicensesScreen(
                             contentDescription = "Back",
                         )
                     }
-                }
+                },
+                scrollBehavior = scrollBehavior,
             )
-        }
+        },
     ) { innerPadding ->
         LibrariesContainer(
             libraries = libraries,
+            contentPadding = innerPadding,
             modifier = Modifier
-                .padding(innerPadding)
-                .fillMaxSize(),
+                .fillMaxSize()
+                .nestedScroll(scrollBehavior.nestedScrollConnection),
         )
     }
 }
