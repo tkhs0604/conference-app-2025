cfb5322c7c2e66e2c86ab5751ca5a7ec2799301b
kitakkun <48154936+kitakkun@users.noreply.github.com>
2025-08-12 20:24:35 +0900

    fix: compilation issue due to overload resolution ambiguity on JVM target

--- core/droidkaigiui/src/commonMain/kotlin/io/github/droidkaigi/confsched/droidkaigiui/extension/WindowInsetsExtensions.kt
+++ core/droidkaigiui/src/commonMain/kotlin/io/github/droidkaigi/confsched/droidkaigiui/extension/WindowInsetsExtensions.kt
@@ -7,3 +7,11 @@ import androidx.compose.foundation.layout.only
 fun WindowInsets.excludeTop(): WindowInsets {
     return this.only(WindowInsetsSides.Horizontal + WindowInsetsSides.Bottom)
 }
+
+val WindowInsets.Companion.Empty: WindowInsets
+    get() = WindowInsets(
+        left = 0,
+        top = 0,
+        right = 0,
+        bottom = 0,
+    )
