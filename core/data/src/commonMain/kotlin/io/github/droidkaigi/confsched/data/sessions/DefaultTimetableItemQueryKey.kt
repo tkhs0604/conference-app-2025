501f07b4b182840b21776ce4c6b780ed8a57d8de
kitakkun <48154936+kitakkun@users.noreply.github.com>
2025-08-16 23:46:59 +0900

    refactor: apply minor manual refactorings for readability

--- core/data/src/commonMain/kotlin/io/github/droidkaigi/confsched/data/sessions/DefaultTimetableItemQueryKey.kt
+++ core/data/src/commonMain/kotlin/io/github/droidkaigi/confsched/data/sessions/DefaultTimetableItemQueryKey.kt
@@ -2,29 +2,25 @@ package io.github.droidkaigi.confsched.data.sessions
 
 import dev.zacsweers.metro.ContributesBinding
 import dev.zacsweers.metro.Inject
-import dev.zacsweers.metro.binding
 import io.github.droidkaigi.confsched.common.scope.TimetableDetailScope
 import io.github.droidkaigi.confsched.model.data.TimetableItemQueryKey
-import io.github.droidkaigi.confsched.model.sessions.TimetableItem
 import io.github.droidkaigi.confsched.model.sessions.TimetableItemId
 import soil.query.QueryId
-import soil.query.QueryKey
 import soil.query.buildQueryKey
 
-@ContributesBinding(TimetableDetailScope::class, binding<TimetableItemQueryKey>())
+@ContributesBinding(TimetableDetailScope::class)
 @Inject
 public class DefaultTimetableItemQueryKey(
-    override val timetableItemId: TimetableItemId,
+    timetableItemId: TimetableItemId,
     private val sessionsApiClient: SessionsApiClient,
     private val dataStore: SessionCacheDataStore,
-) : TimetableItemQueryKey,
-    QueryKey<TimetableItem> by buildQueryKey(
-        id = QueryId(timetableItemId.value),
-        fetch = {
-            val response = dataStore.getCache() ?: sessionsApiClient.sessionsAllResponse()
-            val timetable = response.toTimetable()
-            timetable.timetableItems.first {
-                it.id == timetableItemId
-            }
-        },
-    )
+) : TimetableItemQueryKey by buildQueryKey(
+    id = QueryId(timetableItemId.value),
+    fetch = {
+        val response = dataStore.getCache() ?: sessionsApiClient.sessionsAllResponse()
+        val timetable = response.toTimetable()
+        timetable.timetableItems.first {
+            it.id == timetableItemId
+        }
+    },
+)
--- core/data/src/commonMain/kotlin/io/github/droidkaigi/confsched/data/user/DefaultFavoriteTimetableIdsSubscriptionKey.kt
+++ core/data/src/commonMain/kotlin/io/github/droidkaigi/confsched/data/user/DefaultFavoriteTimetableIdsSubscriptionKey.kt
@@ -2,13 +2,12 @@ package io.github.droidkaigi.confsched.data.user
 
 import dev.zacsweers.metro.ContributesBinding
 import dev.zacsweers.metro.Inject
-import dev.zacsweers.metro.binding
 import io.github.droidkaigi.confsched.data.DataScope
 import io.github.droidkaigi.confsched.model.data.FavoriteTimetableIdsSubscriptionKey
 import soil.query.SubscriptionId
 import soil.query.buildSubscriptionKey
 
-@ContributesBinding(DataScope::class, binding<FavoriteTimetableIdsSubscriptionKey>())
+@ContributesBinding(DataScope::class)
 @Inject
 public class DefaultFavoriteTimetableIdsSubscriptionKey(
     private val userDataStore: UserDataStore,
