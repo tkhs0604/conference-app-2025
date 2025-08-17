8a98835690ccbbedfbe179294c05e2cd2106a4c4
risako070310 <risako070310@gmail.com>
2025-08-18 02:49:53 +0900

    feat: nav to search screen

--- feature/sessions/src/commonMain/kotlin/io/github/droidkaigi/confsched/sessions/SearchScreenContext.kt
+++ feature/sessions/src/commonMain/kotlin/io/github/droidkaigi/confsched/sessions/SearchScreenContext.kt
@@ -4,9 +4,15 @@ import dev.zacsweers.metro.AppScope
 import dev.zacsweers.metro.ContributesGraphExtension
 import io.github.droidkaigi.confsched.common.scope.SearchScope
 import io.github.droidkaigi.confsched.context.ScreenContext
+import io.github.droidkaigi.confsched.model.data.FavoriteTimetableIdsSubscriptionKey
+import io.github.droidkaigi.confsched.model.data.FavoriteTimetableItemIdMutationKey
+import io.github.droidkaigi.confsched.model.data.TimetableQueryKey
 
 @ContributesGraphExtension(SearchScope::class)
 interface SearchScreenContext : ScreenContext {
+    val timetableQueryKey: TimetableQueryKey
+    val favoriteTimetableIdsSubscriptionKey: FavoriteTimetableIdsSubscriptionKey
+
     @ContributesGraphExtension.Factory(AppScope::class)
     fun interface Factory {
         fun createSearchScreenContext(): SearchScreenContext
--- feature/sessions/src/commonMain/kotlin/io/github/droidkaigi/confsched/sessions/SearchScreenRoot.kt
+++ feature/sessions/src/commonMain/kotlin/io/github/droidkaigi/confsched/sessions/SearchScreenRoot.kt
@@ -1,8 +1,35 @@
 package io.github.droidkaigi.confsched.sessions
 
 import androidx.compose.runtime.Composable
+import io.github.droidkaigi.confsched.common.compose.rememberEventFlow
+import io.github.droidkaigi.confsched.droidkaigiui.architecture.SoilDataBoundary
+import io.github.droidkaigi.confsched.model.sessions.TimetableItemId
+import soil.query.compose.rememberQuery
+import soil.query.compose.rememberSubscription
 
 @Composable
 context(screenContext: SearchScreenContext)
-fun SearchScreenRoot() {
+fun SearchScreenRoot(
+    onBackClick: () -> Unit,
+    onTimetableItemClick: (TimetableItemId) -> Unit,
+) {
+    SoilDataBoundary(
+        state1 = rememberQuery(screenContext.timetableQueryKey),
+        state2 = rememberSubscription(screenContext.favoriteTimetableIdsSubscriptionKey),
+    ) { timetable, favoriteIds ->
+        val eventFlow = rememberEventFlow<SearchScreenEvent>()
+
+        val uiState = searchScreenPresenter(
+            eventFlow = eventFlow,
+            timetable = timetable.copy(bookmarks = favoriteIds),
+        )
+
+        SearchScreen(
+            uiState = uiState,
+            onBackClick = onBackClick,
+            onTimetableItemClick = onTimetableItemClick,
+            onEvent = { eventFlow.tryEmit(it) },
+        )
+    }
+
 }
