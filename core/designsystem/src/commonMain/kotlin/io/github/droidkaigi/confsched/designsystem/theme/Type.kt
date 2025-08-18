4aa081db930a8fda1bcf436416b36fcd3b31ee7a
HyungwonJin <hwjin92@gmail.com>
2025-08-18 23:02:39 +0900

    feat: introduce Roboto font family and update typography styles

--- core/designsystem/src/commonMain/kotlin/io/github/droidkaigi/confsched/designsystem/theme/Type.kt
+++ core/designsystem/src/commonMain/kotlin/io/github/droidkaigi/confsched/designsystem/theme/Type.kt
@@ -8,12 +8,22 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 import io.github.droidkaigi.confsched.designsystem.Chango_Regular
 import io.github.droidkaigi.confsched.designsystem.DesignsystemRes
+import io.github.droidkaigi.confsched.designsystem.Roboto_Medium
+import io.github.droidkaigi.confsched.designsystem.Roboto_Regular
 import org.jetbrains.compose.resources.Font
 
 @Composable
 fun AppTypography(): Typography {
     val changoFontFamily = FontFamily(
         Font(DesignsystemRes.font.Chango_Regular, FontWeight.Normal)
+    )
+
+    val robotoRegularFontFamily = FontFamily(
+        Font(DesignsystemRes.font.Roboto_Regular, FontWeight.Normal),
+    )
+
+    val robotoMediumFontFamily = FontFamily(
+        Font(DesignsystemRes.font.Roboto_Medium, FontWeight.Medium),
     )
 
     return Typography(
@@ -59,5 +69,68 @@ fun AppTypography(): Typography {
             lineHeight = 32.sp,
             letterSpacing = 0.sp,
         ),
+        titleLarge = TextStyle(
+            fontFamily = robotoRegularFontFamily,
+            fontWeight = FontWeight.W400,
+            fontSize = 22.sp,
+            lineHeight = 28.sp,
+            letterSpacing = 0.sp,
+        ),
+        titleMedium = TextStyle(
+            fontFamily = robotoMediumFontFamily,
+            fontWeight = FontWeight.W500,
+            fontSize = 16.sp,
+            lineHeight = 24.sp,
+            letterSpacing = 0.15.sp,
+        ),
+        titleSmall = TextStyle(
+            fontFamily = robotoMediumFontFamily,
+            fontWeight = FontWeight.W500,
+            fontSize = 14.sp,
+            lineHeight = 20.sp,
+            letterSpacing = 0.1.sp,
+        ),
+        labelLarge = TextStyle(
+            fontFamily = robotoMediumFontFamily,
+            fontWeight = FontWeight.W500,
+            fontSize = 14.sp,
+            lineHeight = 20.sp,
+            letterSpacing = 0.1.sp,
+        ),
+        labelMedium = TextStyle(
+            fontFamily = robotoMediumFontFamily,
+            fontWeight = FontWeight.W500,
+            fontSize = 12.sp,
+            lineHeight = 16.sp,
+            letterSpacing = 0.5.sp,
+        ),
+        labelSmall = TextStyle(
+            fontFamily = robotoMediumFontFamily,
+            fontWeight = FontWeight.W500,
+            fontSize = 11.sp,
+            lineHeight = 16.sp,
+            letterSpacing = 0.5.sp,
+        ),
+        bodyLarge = TextStyle(
+            fontFamily = robotoRegularFontFamily,
+            fontWeight = FontWeight.W400,
+            fontSize = 16.sp,
+            lineHeight = 24.sp,
+            letterSpacing = 0.5.sp,
+        ),
+        bodyMedium = TextStyle(
+            fontFamily = robotoRegularFontFamily,
+            fontWeight = FontWeight.W400,
+            fontSize = 14.sp,
+            lineHeight = 20.sp,
+            letterSpacing = 0.25.sp,
+        ),
+        bodySmall = TextStyle(
+            fontFamily = robotoRegularFontFamily,
+            fontWeight = FontWeight.W400,
+            fontSize = 12.sp,
+            lineHeight = 16.sp,
+            letterSpacing = 0.4.sp,
+        )
     )
 }
