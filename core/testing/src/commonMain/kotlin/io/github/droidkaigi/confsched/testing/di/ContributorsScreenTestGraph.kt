45c761991b85b68cbc4f0ce49500e5726de96df0
shunm-999 <shunmiyazaki7812@gmail.com>
2025-08-19 12:02:13 +0900

    feat: add ContributorsScreenRobot to ContributorsScreenTestGraph for enhanced testing

--- core/testing/src/commonMain/kotlin/io/github/droidkaigi/confsched/testing/di/ContributorsScreenTestGraph.kt
+++ core/testing/src/commonMain/kotlin/io/github/droidkaigi/confsched/testing/di/ContributorsScreenTestGraph.kt
@@ -3,6 +3,7 @@ package io.github.droidkaigi.confsched.testing.di
 import dev.zacsweers.metro.Provider
 import dev.zacsweers.metro.Provides
 import io.github.droidkaigi.confsched.contributors.ContributorsScreenContext
+import io.github.droidkaigi.confsched.testing.robot.contributors.ContributorsScreenRobot
 
 interface ContributorsScreenTestGraph : ContributorsScreenContext.Factory {
     val contributorsScreenRobotProvider: Provider<ContributorsScreenRobot>
