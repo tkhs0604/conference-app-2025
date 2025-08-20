c2e34cf893add1f82885e1e86b5fcd4b1b553ce8
easyhooon <mraz3068@gmail.com>
2025-08-20 12:32:39 +0900

    feat: uniform systemBarStyle in light/dark mode

--- app-android/src/main/kotlin/io/github/droidkaigi/confsched/MainActivity.kt
+++ app-android/src/main/kotlin/io/github/droidkaigi/confsched/MainActivity.kt
@@ -1,13 +1,18 @@
 package io.github.droidkaigi.confsched
 
+import android.graphics.Color.TRANSPARENT
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        enableEdgeToEdge()
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.dark(scrim = TRANSPARENT),
+            navigationBarStyle = SystemBarStyle.dark(scrim = TRANSPARENT)
+        )
         super.onCreate(savedInstanceState)
 
         with(appGraph) {
